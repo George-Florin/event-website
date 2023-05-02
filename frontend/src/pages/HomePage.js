2ddf1429df2c54f1157babe4a6249483c5156438
George-Florin Petrila <floringpetrila@gmail.com>
2023-05-02 17:33:09 +0300

    Initial commit

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import PageContent from "../components/PageContent";
+
+const HomePage = () => {
+  return (
+    <PageContent title="Welcome!">
+      <p>Check out our events</p>
+    </PageContent>
+  );
+};
+
+export default HomePage;
